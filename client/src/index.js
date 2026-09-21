bcba3bfb23a253b2b5682dc969a68ea6264961b1
alioukande <kandealiou687@gmail.com>
2026-09-21 11:14:10 +0100

    Ajout JS bootstrap pour menu mobile

--- client/src/index.js
+++ client/src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
 
 import App from "./App";
  import Reservations from "./components/Reservations";
